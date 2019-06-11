078b8f403a34ab76b249b7bbc231a2c42bd31fbb
ssoltau123 <susansoltau@Susans-MacBook-Pro.local>
2019-06-11 11:49:00 -0700

    Final Stats. Paired Wilcox on Pre and Post wet up Fluxes
    Krustal Wallis and Dunn on Difference over time
    
    Is the Birch effect happening and does the magnitude of the effect change over time?

--- Gas Flux Stats.R
+++ Gas Flux Stats.R
@@ -1,4 +1,4 @@
-GasMoist <- read.csv("~/Desktop/GasMoist.csv")
+GasMoist <- read.csv("~/Desktop/BEMS R Studio/Data/GasMoist.csv")
 View(GasMoist)
 
 #Read in File
@@ -42,93 +42,65 @@ CFRateSAT<-subset(CFRate, Treatment=="SAT")
 ULRate4Pre<-subset(ULRate4, Sample.Date=="6"|Sample.Date=="12"|Sample.Date=="18"|Sample.Date=="24")
 ULRate4Post<-subset(ULRate4, Sample.Date=="7"|Sample.Date=="13"|Sample.Date=="19"|Sample.Date=="25")
 
-ULRate4Pre$Post<-ULRate4Post$Gas.Flux...mg.CO2.C.kg.OD.soil.d
+ULRate4Pre$Post<-ULRate4Post$Gas.Flux...mg.CO2.C.kg.OD.soil.d.
+ULRate4Pre$PostN2O<-ULRate4Post$Gas.Flux...µg.N2O.N.kg.OD.d.
+ULRate4Pre$PostCH4<-ULRate4Post$Gas.Flux...µg.CH4.C.kg.OD.soil.d.
 
 
 DMRate4Pre<-subset(DMRate4, Sample.Date=="6"|Sample.Date=="12"|Sample.Date=="18"|Sample.Date=="24")
 DMRate4Post<-subset(DMRate4, Sample.Date=="7"|Sample.Date=="13"|Sample.Date=="19"|Sample.Date=="25")
 
 DMRate4Pre$Post<-DMRate4Post$Gas.Flux...mg.CO2.C.kg.OD.soil.d
+DMRate4Pre$PostN2O<-DMRate4Post$Gas.Flux...µg.N2O.N.kg.OD.d.
+DMRate4Pre$PostCH4<-DMRate4Post$Gas.Flux...µg.CH4.C.kg.OD.soil.d.
 
 BFRate4Pre<-subset(BFRate4, Sample.Date=="6"|Sample.Date=="12"|Sample.Date=="18"|Sample.Date=="24")
 BFRate4Post<-subset(BFRate4, Sample.Date=="7"|Sample.Date=="13"|Sample.Date=="19"|Sample.Date=="25")
 
 BFRate4Pre$Post<-BFRate4Post$Gas.Flux...mg.CO2.C.kg.OD.soil.d
+BFRate4Pre$PostN2O<-BFRate4Post$Gas.Flux...µg.N2O.N.kg.OD.d.
+BFRate4Pre$PostCH4<-BFRate4Post$Gas.Flux...µg.CH4.C.kg.OD.soil.d.
 
 CFRate4Pre<-subset(CFRate4, Sample.Date=="6"|Sample.Date=="12"|Sample.Date=="18"|Sample.Date=="24")
 CFRate4Post<-subset(CFRate4, Sample.Date=="7"|Sample.Date=="13"|Sample.Date=="19"|Sample.Date=="25")
 
 CFRate4Pre$Post<-CFRate4Post$Gas.Flux...mg.CO2.C.kg.OD.soil.d
+CFRate4Pre$PostN2O<-CFRate4Post$Gas.Flux...µg.N2O.N.kg.OD.d.
+CFRate4Pre$PostCH4<-CFRate4Post$Gas.Flux...µg.CH4.C.kg.OD.soil.d.
 
 #Subset Pre and Post for 12 Cycle
 
 ULRate12Pre<-subset(ULRate12, Sample.Date=="2"|Sample.Date=="4"|Sample.Date=="6"|Sample.Date=="8"|Sample.Date=="10"|Sample.Date=="12"|Sample.Date=="14"|Sample.Date=="16"|Sample.Date=="18"|Sample.Date=="20"|Sample.Date=="22"|Sample.Date=="24")
 ULRate12Post<-subset(ULRate12, Sample.Date=="3"|Sample.Date=="5"|Sample.Date=="7"|Sample.Date=="9"|Sample.Date=="11"|Sample.Date=="13"|Sample.Date=="15"|Sample.Date=="17"|Sample.Date=="19"|Sample.Date=="21"|Sample.Date=="23"|Sample.Date=="25")
 
-ULRate12Pre$Post<-ULRate12Post$Gas.Flux...mg.CO2.C.kg.OD.soil.d
+ULRate12Pre$Post<-ULRate12Post$Gas.Flux...mg.CO2.C.kg.OD.soil.d.
+ULRate12Pre$PostN2O<-ULRate4Post$Gas.Flux...µg.N2O.N.kg.OD.d.
+ULRate12Pre$PostCH4<-ULRate4Post$Gas.Flux...µg.CH4.C.kg.OD.soil.d.
 
 DMRate12Pre<-subset(DMRate12, Sample.Date=="2"|Sample.Date=="4"|Sample.Date=="6"|Sample.Date=="8"|Sample.Date=="10"|Sample.Date=="12"|Sample.Date=="14"|Sample.Date=="16"|Sample.Date=="18"|Sample.Date=="20"|Sample.Date=="22"|Sample.Date=="24")
 DMRate12Post<-subset(DMRate12, Sample.Date=="3"|Sample.Date=="5"|Sample.Date=="7"|Sample.Date=="9"|Sample.Date=="11"|Sample.Date=="13"|Sample.Date=="15"|Sample.Date=="17"|Sample.Date=="19"|Sample.Date=="21"|Sample.Date=="23"|Sample.Date=="25")
 
 DMRate12Pre$Post<-DMRate12Post$Gas.Flux...mg.CO2.C.kg.OD.soil.d
+DMRate12Pre$PostN2O<-DMRate4Post$Gas.Flux...µg.N2O.N.kg.OD.d.
+DMRate12Pre$PostCH4<-DMRate4Post$Gas.Flux...µg.CH4.C.kg.OD.soil.d.
 
 BFRate12Pre<-subset(BFRate12, Sample.Date=="2"|Sample.Date=="4"|Sample.Date=="6"|Sample.Date=="8"|Sample.Date=="10"|Sample.Date=="12"|Sample.Date=="14"|Sample.Date=="16"|Sample.Date=="18"|Sample.Date=="20"|Sample.Date=="22"|Sample.Date=="24")
 BFRate12Post<-subset(BFRate12, Sample.Date=="3"|Sample.Date=="5"|Sample.Date=="7"|Sample.Date=="9"|Sample.Date=="11"|Sample.Date=="13"|Sample.Date=="15"|Sample.Date=="17"|Sample.Date=="19"|Sample.Date=="21"|Sample.Date=="23"|Sample.Date=="25")
 
 BFRate12Pre$Post<-BFRate12Post$Gas.Flux...mg.CO2.C.kg.OD.soil.d
+BFRate12Pre$PostN2O<-BFRate4Post$Gas.Flux...µg.N2O.N.kg.OD.d.
+BFRate12Pre$PostCH4<-BFRate4Post$Gas.Flux...µg.CH4.C.kg.OD.soil.d.
 
 CFRate12Pre<-subset(CFRate12, Sample.Date=="2"|Sample.Date=="4"|Sample.Date=="6"|Sample.Date=="8"|Sample.Date=="10"|Sample.Date=="12"|Sample.Date=="14"|Sample.Date=="16"|Sample.Date=="18"|Sample.Date=="20"|Sample.Date=="22"|Sample.Date=="24")
 CFRate12Post<-subset(CFRate12, Sample.Date=="3"|Sample.Date=="5"|Sample.Date=="7"|Sample.Date=="9"|Sample.Date=="11"|Sample.Date=="13"|Sample.Date=="15"|Sample.Date=="17"|Sample.Date=="19"|Sample.Date=="21"|Sample.Date=="23"|Sample.Date=="25")
 
-CFRate12Pre$Post<-CFRate12Post$Gas.Flux...mg.CO2.C.kg.OD.soil.d
-
-#Test for Normality (#** needs Log T) CO2
-
-shapiro.test(ULRate4Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d.) #**
- 
-shapiro.test(ULRate4Pre$Post) 
-
-shapiro.test(DMRate4Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d.) #**
-  
-shapiro.test(DMRate4Pre$Post) 
-
-shapiro.test(BFRate4Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d.) 
-
-shapiro.test(BFRate4Pre$Post) 
-
-shapiro.test(CFRate4Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d.) #**
-
-shapiro.test(CFRate4Pre$Post) 
-
-shapiro.test(ULRate12Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d.) #**
-
-shapiro.test(ULRate12Pre$Post) #**
-
-shapiro.test(DMRate12Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d.) #**
-
-shapiro.test(DMRate12Pre$Post) #**
-
-shapiro.test(BFRate12Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d.) #**
-
-shapiro.test(BFRate12Pre$Post) #**
-
-shapiro.test(CFRate12Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d.) #**
-
-shapiro.test(CFRate12Pre$Post) #**
-
-#Log Transform (Did for all 12 cycle)
-
-ULRate4Pre$logPre<-log10(ULRate4Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d.+10)
-shapiro.test(ULRate4Pre$logPre)
-
-DMRate4Pre$logPre<-log10(DMRate4Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d.+10)
-shapiro.test(DMRate4Pre$logPre)
-
-CFRate4Pre$logPre<-log10(CFRate4Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d.+10)
-shapiro.test(CFRate4Pre$logPre)
+CFRate12Pre$Post<-CFRate12Post$Gas.Flux...mg.CO2.C.kg.OD.soil.d.
+CFRate12Pre$PostN2O<-CFRate4Post$Gas.Flux...µg.N2O.N.kg.OD.d.
+CFRate12Pre$PostCH4<-CFRate4Post$Gas.Flux...µg.CH4.C.kg.OD.soil.d.
 
 
 #Paired T Test for Cycle (Wilcoxxn non parametric approach) Pre and Post CO2, N2O, CH4
+#Is the Birch effect happening???  A sig diff between pre and post wet up gas fluxes
 wilcox.test(ULRate4Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d., ULRate4Post$Gas.Flux...mg.CO2.C.kg.OD.soil.d., paired=TRUE) #p-value = 0.0004883
 wilcox.test(DMRate4Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d., DMRate4Post$Gas.Flux...mg.CO2.C.kg.OD.soil.d., paired=TRUE) #p-value = 0.002441 
 wilcox.test(BFRate4Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d., BFRate4Post$Gas.Flux...mg.CO2.C.kg.OD.soil.d., paired=TRUE) #p-value = 0.0004883
@@ -161,57 +133,117 @@ wilcox.test(CFRate12Pre$Gas.Flux...µg.CH4.C.kg.OD.soil.d., CFRate12Post$Gas.Flu
 
 #non parametric tests would be preferred (p value way too low-most likely not fixed by log T)
 
-install.packages("onewaytests")
-
 #Krustal Wallis and Dunn Test CO2, N2O, and CH4 (4 Cycle)
 install.packages("FSA")
+install.packages ("car")
+install.packages ("lsmeans")
+install.packages("ggplot2")
 library(FSA)
-citation('FSA')
-citation('ggplot2')
 
-kruskal.test(Gas.Flux...mg.CO2.C.kg.OD.soil.d. ~ Sample.Date, data = ULRate4) #p-value = 5.582e-05
-dunnTest(Gas.Flux...mg.CO2.C.kg.OD.soil.d. ~ Sample.Date, data = ULRate4, method="bh") #1 and 2 SD were sig.
+install.packages("dunn.test")
 
-kruskal.test(Gas.Flux...mg.CO2.C.kg.OD.soil.d. ~ Sample.Date, data = DMRate4) #p-value = 5.817e-05
-dunnTest(Gas.Flux...mg.CO2.C.kg.OD.soil.d. ~ Sample.Date, data = DMRate4, method="bh") #Just 1 SD is sig
+#Difference between pre and post and new column
 
-kruskal.test(Gas.Flux...mg.CO2.C.kg.OD.soil.d. ~ Sample.Date, data = BFRate4) #p-value = 4.301e-05
-dunnTest(Gas.Flux...mg.CO2.C.kg.OD.soil.d. ~ Sample.Date, data = BFRate4, method="bh") #Just 1 SD is sig
+ULRate4Pre$Dif <- ULRate4Pre$Post - ULRate4Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d
+DMRate4Pre$Dif <- DMRate4Pre$Post -DMRate4Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d
+BFRate4Pre$Dif <- BFRate4Pre$Post - BFRate4Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d
+CFRate4Pre$Dif <- CFRate4Pre$Post - CFRate4Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d
 
-kruskal.test(Gas.Flux...mg.CO2.C.kg.OD.soil.d. ~ Sample.Date, data = CFRate4) #p-value = 0.0002674
-dunnTest(Gas.Flux...mg.CO2.C.kg.OD.soil.d. ~ Sample.Date, data = CFRate4, method="bh") #Just 1 SD is sig, less so than BF or DM
+ULRate4Pre$DifN2O <- ULRate4Pre$Post - ULRate4Pre$Gas.Flux...µg.N2O.N.kg.OD.d.
+DMRate4Pre$DifN2O <- DMRate4Pre$Post -DMRate4Pre$Gas.Flux...µg.N2O.N.kg.OD.d.
+BFRate4Pre$DifN2O <- BFRate4Pre$Post - BFRate4Pre$Gas.Flux...µg.N2O.N.kg.OD.d.
+CFRate4Pre$DifN2O <- CFRate4Pre$Post - CFRate4Pre$Gas.Flux...µg.N2O.N.kg.OD.d.
 
-kruskal.test(Gas.Flux...µg.CH4.C.kg.OD.soil.d. ~ Sample.Date, data = ULRate4) #p-value = 4.05e-05
-dunnTest(Gas.Flux...µg.CH4.C.kg.OD.soil.d. ~ Sample.Date, data = ULRate4, method="bh") #1 and 2 SD were sig.
+ULRate4Pre$DifCH4 <- ULRate4Pre$Post - ULRate4Pre$Gas.Flux...µg.CH4.C.kg.OD.soil.d.
+DMRate4Pre$DifCH4 <- DMRate4Pre$Post -DMRate4Pre$Gas.Flux...µg.CH4.C.kg.OD.soil.d.
+BFRate4Pre$DifCH4 <- BFRate4Pre$Post - BFRate4Pre$Gas.Flux...µg.CH4.C.kg.OD.soil.d.
+CFRate4Pre$DifCH4 <- CFRate4Pre$Post - CFRate4Pre$Gas.Flux...µg.CH4.C.kg.OD.soil.d.
 
-kruskal.test(Gas.Flux...µg.CH4.C.kg.OD.soil.d. ~ Sample.Date, data = DMRate4) #p-value = 5.817e-05
-dunnTest(Gas.Flux...µg.CH4.C.kg.OD.soil.d. ~ Sample.Date, data = DMRate4, method="bh") #Just 1 SD is sig
+ULRate12Pre$Dif <- ULRate12Pre$Post - ULRate12Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d
+DMRate12Pre$Dif <- DMRate12Pre$Post -DMRate12Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d
+BFRate12Pre$Dif <- BFRate12Pre$Post - BFRate12Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d
+CFRate12Pre$Dif <- CFRate12Pre$Post - CFRate12Pre$Gas.Flux...mg.CO2.C.kg.OD.soil.d
 
-kruskal.test(Gas.Flux...µg.CH4.C.kg.OD.soil.d. ~ Sample.Date, data = BFRate4) #p-value = 4.301e-05
-dunnTest(Gas.Flux...µg.CH4.C.kg.OD.soil.d. ~ Sample.Date, data = BFRate4, method="bh") #Just 1 SD is sig
+ULRate12Pre$DifN2O <- ULRate12Pre$Post - ULRate12Pre$Gas.Flux...µg.N2O.N.kg.OD.d.
+DMRate12Pre$DifN2O <- DMRate12Pre$Post -DMRate12Pre$Gas.Flux...µg.N2O.N.kg.OD.d.
+BFRate12Pre$DifN2O <- BFRate12Pre$Post - BFRate12Pre$Gas.Flux...µg.N2O.N.kg.OD.d.
+CFRate12Pre$DifN2O <- CFRate12Pre$Post - CFRate12Pre$Gas.Flux...µg.N2O.N.kg.OD.d.
 
-kruskal.test(Gas.Flux...µg.CH4.C.kg.OD.soil.d. ~ Sample.Date, data = CFRate4) #p-value = 0.0002674
-dunnTest(Gas.Flux...µg.CH4.C.kg.OD.soil.d. ~ Sample.Date, data = CFRate4, method="bh") #Just 1 SD is sig, less so than BF or DM
+ULRate12Pre$DifCH12 <- ULRate12Pre$Post - ULRate12Pre$Gas.Flux...µg.CH4.C.kg.OD.soil.d.
+DMRate12Pre$DifCH12 <- DMRate12Pre$Post -DMRate12Pre$Gas.Flux...µg.CH4.C.kg.OD.soil.d.
+BFRate12Pre$DifCH12 <- BFRate12Pre$Post - BFRate12Pre$Gas.Flux...µg.CH4.C.kg.OD.soil.d.
+CFRate12Pre$DifCH12 <- CFRate12Pre$Post - CFRate12Pre$Gas.Flux...µg.CH4.C.kg.OD.soil.d.
 
-#Krustal Wallis and Dunn Test CO2, N2O, and CH4 (12 cycle)
-
-kruskal.test(Gas.Flux...mg.CO2.C.kg.OD.soil.d. ~ Sample.Date, data = ULRate12) #p-value = 2.012e-06
-dunnTest(Gas.Flux...mg.CO2.C.kg.OD.soil.d. ~ Sample.Date, data = ULRate12, method="bh") #no sig
-
-kruskal.test(Gas.Flux...mg.CO2.C.kg.OD.soil.d. ~ Sample.Date, data = DMRate12) #p-value = 9.83e-06
-dunnTest(Gas.Flux...mg.CO2.C.kg.OD.soil.d. ~ Sample.Date, data = DMRate12, method="bh") #no sig
-
-kruskal.test(Gas.Flux...mg.CO2.C.kg.OD.soil.d. ~ Sample.Date, data = BFRate12) #p-value = 3.037e-06
-dunnTest(Gas.Flux...mg.CO2.C.kg.OD.soil.d. ~ Sample.Date, data = BFRate12, method="bh") #Just 6 SD is sig (half way)
-
-kruskal.test(Gas.Flux...mg.CO2.C.kg.OD.soil.d. ~ Sample.Date, data = CFRate12) # p-value = 2.124e-06
-dunnTest(Gas.Flux...mg.CO2.C.kg.OD.soil.d. ~ Sample.Date, data = CFRate12, method="bh") #Just 2 SD is sig
+View((ULRate12Pre))
+#Krustal Wallis and Dunn Test on Difference 
+#Is there a change in the magnitude of the Birch effect over time??
 
 
+# 4 Cycle
+
+kruskal.test(Dif ~ Sample.Date, data = ULRate4Pre) #p-value = 0.01556
+dunnTest(Dif ~ Sample.Date, data = ULRate4Pre, method="sidak") #sig between 6 and 18
+
+kruskal.test(Dif ~ Sample.Date, data = DMRate4Pre) #p-value = 0.02374
+dunnTest(Dif ~ Sample.Date, data = DMRate4Pre, method="sidak") #sig between 18 and 24
+
+kruskal.test(Dif ~ Sample.Date, data = BFRate4Pre) #N.S
+dunnTest(Dif ~ Sample.Date, data = BFRate4Pre)
+
+kruskal.test(Dif ~ Sample.Date, data = CFRate4Pre) #p-value = 0.0232
+dunnTest(Dif ~ Sample.Date, data = CFRate4Pre, method="sidak") #sig between 6 and 18
 
 
 
+kruskal.test(DifN2O ~ Sample.Date, data = ULRate4Pre) #p-value = 0.01556
+dunnTest(DifN2O ~ Sample.Date, data = ULRate4Pre, method="sidak") #sig between 6 and 18
+
+kruskal.test(DifN2O ~ Sample.Date, data = DMRate4Pre) #p-value=0.02374
+dunnTest(DifN2O ~ Sample.Date, data = DMRate4Pre,method="sidak") #Sig between 18 and 24
+
+kruskal.test(DifN2O ~ Sample.Date, data = BFRate4Pre) #p-value=0.03781
+dunnTest(DifN2O ~ Sample.Date, data = BFRate4Pre, method="sidak") #N.S.
+
+kruskal.test(DifN2O ~ Sample.Date, data = CFRate4Pre) #p-value = 0.01556
+dunnTest(DifN2O ~ Sample.Date, data = CFRate4Pre) #sig between 6 and 18
 
 
 
+kruskal.test(DifCH4 ~ Sample.Date, data = ULRate4Pre) #p-value = 0.02374
+dunnTest(DifCH4 ~ Sample.Date, data = ULRate4Pre, method="sidak") #sig between 12 and 18
 
+
+kruskal.test(DifCH4 ~ Sample.Date, data = BFRate4Pre) #p-value = 0.03781
+dunnTest(DifCH4 ~ Sample.Date, data = BFRate4Pre, method="sidak") #sig between 18 and 24
+
+
+#12 Cycle
+
+kruskal.test(Dif ~ Sample.Date, data = ULRate12Pre) #p-value = 0.001052
+dunnTest(Dif ~ Sample.Date, data = ULRate12Pre, method="sidak") #sig between 8 and 12 and between 4 and 12
+
+kruskal.test(Dif ~ Sample.Date, data = DMRate12Pre) #p-value= 0.002659
+dunnTest(Dif ~ Sample.Date, data = DMRate12Pre, method="sidak") #sig between 10 and 16 and between
+
+kruskal.test(Dif ~ Sample.Date, data = BFRate12Pre) #p-value=0.0008179
+dunnTest(Dif ~ Sample.Date, data = BFRate12Pre, method="sidak") #sig between 12 and 2 
+
+kruskal.test(Dif ~ Sample.Date, data = CFRate12Pre) #p-value = 0.003694
+dunnTest(Dif ~ Sample.Date, data = CFRate12Pre, method="sidak") #sig between 12 and 8 and between 12 and 4
+
+
+
+kruskal.test(DifN2O ~ Sample.Date, data = ULRate12Pre) #p-value=0.0005088
+dunnTest(DifN2O ~ Sample.Date, data = ULRate12Pre, method="sidak") #sig between 18 and 4
+
+kruskal.test(DifN2O ~ Sample.Date, data = DMRate12Pre) #p-value=0.001464
+dunnTest(DifN2O ~ Sample.Date, data = DMRate12Pre,method="sidak") #sig between 18 and 2
+
+kruskal.test(DifN2O ~ Sample.Date, data = BFRate12Pre) #p-value=0.001063
+dunnTest(DifN2O ~ Sample.Date, data = BFRate12Pre, method="sidak") #sig between 12 and 2
+
+kruskal.test(DifN2O ~ Sample.Date, data = CFRate12Pre) #0.0005665
+dunnTest(DifN2O ~ Sample.Date, data = CFRate12Pre, method="sidak") #sig between 18 and 8 and between 18 and 2 
+
+
+#CH4 not sig in Wilcox so no follow up for 12 cycle
